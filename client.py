a7996cbd71b95e6fdf3bb32d0155ed987b41b7d3
Darren Lee <56071442+darrenleeleelee1@users.noreply.github.com>
2021-04-11 00:11:46 +0800

    delete client.py no longer need

--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-files = [('files', ('README', open('./README.md', 'rb')))]
-# header = {'Content-Type': 'multipart/form-data'}
-response = requests.post('http://127.0.0.1:8000/pdicom/123', files=files)
-# response = requests.post('http://127.0.0.1:8000/files', files={'fir': open('./README.md', 'rb')})
-
-print(response.headers)
