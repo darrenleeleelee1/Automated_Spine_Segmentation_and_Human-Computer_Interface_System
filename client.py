dedf902f8de29184cb469d8798fef87ffff838d6
chien <changping415@gmail.com>
2021-04-12 14:18:50 +0800

    solve conflict

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import requests
+#post
 files = [('files', ('README', open('./README.md', 'rb')))]
 # header = {'Content-Type': 'multipart/form-data'}
 response = requests.post('http://127.0.0.1:8000/pdicom/123', files=files)
