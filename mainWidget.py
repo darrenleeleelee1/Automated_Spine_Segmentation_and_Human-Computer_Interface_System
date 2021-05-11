b5601f2292ca928f95f6565ad636d4cdb04f42b7
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2021-05-11 17:11:46 +0800

    broken so sad

--- mainWidget.py
+++ mainWidget.py
@@ -29,8 +29,14 @@ class initialWidget(QtWidgets.QMainWindow):
         self.pt_list.sort()
         for ptid in self.pt_list:
             self.ui.no_list.addItem(ptid)
-
+        
+        self.tmp_cnt = 0    
+        
+        # 控制工具列現在選擇的工具為: mouse(defalut), pen, angle, ruler, move 
+        self.tool_lock = "mouse"
         self.backend()
+
+
         def moveWindow(e):
             if self.isMaximized() == False:  # Not maximized
                 if e.buttons() == Qt.LeftButton:
@@ -57,74 +63,108 @@ class initialWidget(QtWidgets.QMainWindow):
         self.ui.patient_list.itemClicked.connect(self.patient_listItemClicked)
         self.ui.input_no.setCompleter(completer)  # 搜尋紀錄
         self.linkPage2Array() # 將影像處理頁面預設有五頁
-        self.ui.pushButton_angle.clicked.connect(self.pushButtonAngleClicked)
-        self.ui.pushButton_add_pic.clicked.connect(self.pushButtonAddPicClicked)
+        self.ui.pushButton_angle.clicked.connect(self.pushButtonAngleClicked) # 角度按鈕連結
+        self.ui.pushButton_add_pic.clicked.connect(self.pushButtonAddPicClicked) # 加照片按鈕連結
+        self.ui.pushButton_pen.clicked.connect(self.pushButtonPenClicked)
 #工具列-----------------------------------------------------------------------------------------------------------
     def picMouseReleased(self, event, _i, _j):
-        if event.button()== Qt.LeftButton:
-            if(self.pic_clicked[_i][_j]):
-                self.pic_released[_i][_j] = True
-            else:
-                self.pic_released[_i][_j] = False
-        print("clicked:%d, released:%d" % (self.pic_clicked[_i][_j], self.pic_released[_i][_j]))
+        if(self.tool_lock == 'mouse'):
+            return
+        elif(self.tool_lock == 'angle'):
+            if event.button()== Qt.LeftButton:
+                if(self.pic_clicked[_i][_j]):
+                    self.pic_released[_i][_j] = True
+                else:
+                    self.pic_released[_i][_j] = False
+            # print("clicked:%d, released:%d" % (self.pic_clicked[_i][_j], self.pic_released[_i][_j]))
+        elif(self.tool_lock == 'pen'):
+            return
+
 
     def picMousePressed(self, event, _i, _j):
         self.pic_ith = _i
         self.pic_jth = _j
-        # self.pic_start_x[self.pic_ith][self.pic_jth] = None
-        # self.pic_start_y[self.pic_ith][self.pic_jth] = None
-        if event.button() == QtCore.Qt.LeftButton:
-            if(not self.pic_clicked[_i][_j]):
-                self.pic_clicked[_i][_j] = True
+        if(self.tool_lock == 'mouse'):
+            return
+        if(self.tool_lock == 'angle'):
+            if event.button() == QtCore.Qt.LeftButton:
+                if(not self.pic_clicked[_i][_j]):
+                    self.pic_clicked[_i][_j] = True
+                    self.pic_start_x[self.pic_ith][self.pic_jth] = event.pos().x()
+                    self.pic_start_y[self.pic_ith][self.pic_jth] = event.pos().y()
+                else:
+                    self.pic_clicked[_i][_j] = False
+            # print("clicked:%d, released:%d" % (self.pic_clicked[_i][_j], self.pic_released[_i][_j]))
+        elif(self.tool_lock == 'pen'):
+            if event.button() == QtCore.Qt.LeftButton:
+                self.pic_end_x[_i][_j] = event.x()
+                self.pic_end_y[_i][_j] = event.y()
                 self.pic_start_x[self.pic_ith][self.pic_jth] = event.pos().x()
                 self.pic_start_y[self.pic_ith][self.pic_jth] = event.pos().y()
-            else:
-                self.pic_clicked[_i][_j] = False
-        print("clicked:%d, released:%d" % (self.pic_clicked[_i][_j], self.pic_released[_i][_j]))
-                
-        # if(self.pic_start_x[self.pic_ith][self.pic_jth] != None):
-            # print("clicked pic[%s][%s] at (%d, %d)" % (_i, _j, self.pic_start_x[self.pic_ith][self.pic_jth], self.pic_start_y[self.pic_ith][self.pic_jth])) # 說明是哪個pic[_i][_j]被按
-
-    def picMouseMove(self, event):
+    def picMouseMove(self, event, _i, _j):
         # distance_from_center = round(((event.y() - self.pic_start_y[self.pic_ith][self.pic_jth])**2 + (event.x() - self.pic_start_x[self.pic_ith][self.pic_jth])**2)**0.5)
         # self.label.setText('Coordinates: ( %d : %d )' % (event.x(), event.y()) + "Distance from center: " + str(distance_from_center))       
         # print(distance_from_center)
-        # q = QtGui.QPainter(self.pic[self.pic_ith][self.pic_jth])
-        # q.drawLine(event.x(), event.y(), self.pic_start_x[self.pic_ith][self.pic_jth], self.pic_start_y[self.pic_ith][self.pic_jth])
-        if event.buttons() == QtCore.Qt.NoButton:
-            if(self.pic_clicked[self.pic_ith][self.pic_jth] and self.pic_released[self.pic_ith][self.pic_jth]):
-                self.pic_end_x[self.pic_ith][self.pic_jth] = event.x()
-                self.pic_end_y[self.pic_ith][self.pic_jth] = event.y()
-        elif event.buttons() == QtCore.Qt.LeftButton:
-            if(self.pic_clicked[self.pic_ith][self.pic_jth] and not self.pic_released[self.pic_ith][self.pic_jth]):
-                self.pic_middle_x[self.pic_ith][self.pic_jth] = self.pic_end_x[self.pic_ith][self.pic_jth] = event.x()
-                self.pic_middle_y[self.pic_ith][self.pic_jth] = self.pic_end_y[self.pic_ith][self.pic_jth] = event.y()
+        if(self.tool_lock == 'mouse'):
+            return
+        elif(self.tool_lock == 'angle'):
+            if event.buttons() == QtCore.Qt.NoButton:
+                if(self.pic_clicked[self.pic_ith][self.pic_jth] and self.pic_released[self.pic_ith][self.pic_jth]):
+                    self.pic_end_x[self.pic_ith][self.pic_jth] = event.x()
+                    self.pic_end_y[self.pic_ith][self.pic_jth] = event.y()
+            elif event.buttons() == QtCore.Qt.LeftButton:
+                if(self.pic_clicked[self.pic_ith][self.pic_jth] and not self.pic_released[self.pic_ith][self.pic_jth]):
+                    self.pic_middle_x[self.pic_ith][self.pic_jth] = self.pic_end_x[self.pic_ith][self.pic_jth] = event.x()
+                    self.pic_middle_y[self.pic_ith][self.pic_jth] = self.pic_end_y[self.pic_ith][self.pic_jth] = event.y()
+        elif(self.tool_lock == 'pen'):
+            self.pic_start_x[self.pic_ith][self.pic_jth] = self.pic_end_x[_i][_j]
+            self.pic_start_y[self.pic_ith][self.pic_jth] = self.pic_end_y[_i][_j]
+            self.pic_end_x[_i][_j] = event.x() 
+            self.pic_end_y[_i][_j] = event.y()
         
         self.update()
 
     def picPaint(self, event, pixmap, _i, _j):
-        """
-        print("start(%d, %d)" % (self.pic_start_x[_i][_j], self.pic_start_y[_i][_j]))
-        print("middle(%d, %d)" % (self.pic_middle_x[_i][_j], self.pic_middle_y[_i][_j]))
-        print("end(%d, %d)" % (self.pic_end_x[_i][_j], self.pic_end_y[_i][_j]))
-        """
         q = QtGui.QPainter(self.pic[_i][_j])
-        p = QtGui.QPen()
-        p.setWidth(6)
-        q.setPen(p)
         q.drawPixmap(0, 0, 512, 512, pixmap)
-        q.drawLine(self.pic_middle_x[_i][_j], self.pic_middle_y[_i][_j], self.pic_start_x[_i][_j], self.pic_start_y[_i][_j])
-        q.drawLine(self.pic_end_x[_i][_j], self.pic_end_y[_i][_j], self.pic_middle_x[_i][_j], self.pic_middle_y[_i][_j])
+        p = QtGui.QPainter(self.transparent_pix[_i][_j])
+        if(self.tool_lock == 'mouse'):
+            return
+        elif(self.tool_lock == 'angle'):
+            self.pic[_i][_j].setMouseTracking(True)
+            pen = QtGui.QPen()
+            pen.setWidth(6)
+            q.setPen(pen)
+            q.drawLine(self.pic_middle_x[_i][_j], self.pic_middle_y[_i][_j], self.pic_start_x[_i][_j], self.pic_start_y[_i][_j])
+            q.drawLine(self.pic_end_x[_i][_j], self.pic_end_y[_i][_j], self.pic_middle_x[_i][_j], self.pic_middle_y[_i][_j])
+            print("Count #%d:" % self.tmp_cnt)
+            print("tool: %s", self.tool_lock)
+            print("start(%d, %d)" % (self.pic_start_x[_i][_j], self.pic_start_y[_i][_j]))
+            print("middle(%d, %d)" % (self.pic_middle_x[_i][_j], self.pic_middle_y[_i][_j]))
+            print("end(%d, %d)" % (self.pic_end_x[_i][_j], self.pic_end_y[_i][_j]))
+            self.tmp_cnt += 1
+        elif(self.tool_lock == 'pen'):
+            self.pic[_i][_j].setMouseTracking(False)
+            pen = QtGui.QPen()
+            pen.setWidth(6)
+            p.setPen(pen)
+            p.drawLine(self.pic_end_x[_i][_j], self.pic_end_y[_i][_j], self.pic_start_x[_i][_j], self.pic_start_y[_i][_j])
+        q.drawPixmap(0, 0, self.transparent_pix[_i][_j])
+        
         q.end()
 
+#按鈕連結處--------------------------------------------------------------------------------------------------------
+
     def pushButtonAngleClicked(self):
-        return       
+        self.tool_lock = 'angle'
 
     def pushButtonAddPicClicked(self):
         fileName1, filetype = QFileDialog.getOpenFileName(self,"選取檔案","/Users/user/Documents/畢專/dicom_data","All Files (*);;Text Files (*.txt)")  #設定副檔名過濾,注意用雙分號間隔
         print(filetype)
         # fileName2, ok2 = QFileDialog.getSaveFileName(self,"檔案儲存","./","All Files (*);;Text Files (*.txt)")
 
+    def pushButtonPenClicked(self):
+        self.tool_lock = 'pen'
 
     
 
@@ -232,7 +272,7 @@ class initialWidget(QtWidgets.QMainWindow):
         self.pic[i][j].setGeometry(QtCore.QRect(0, 0, 400, 500))
         self.pic[i][j].mousePressEvent = lambda pressed: self.picMousePressed(pressed, i, j) # 讓每個pic的mousePressEvent可以傳出告訴自己是誰
         self.pic[i][j].mouseReleaseEvent = lambda released: self.picMouseReleased(released, i, j)
-        self.pic[i][j].mouseMoveEvent = self.picMouseMove
+        self.pic[i][j].mouseMoveEvent = lambda moved: self.picMouseMove(moved, i, j)
         self.pic[i][j].paintEvent = lambda painted: self.picPaint(painted, QtGui.QPixmap(qimage), i, j)
         
     def linkPage2Array(self, MAXIMUM_PAGE = 5, MAXIMUM_PIC = 4):
@@ -265,13 +305,20 @@ class initialWidget(QtWidgets.QMainWindow):
             for j in range(1, (MAXIMUM_PIC + 1)):
                 exec("%s[%d][%d] = %s_%d_%d" % (var_array_pic, i, j, var_pic, i, j))
                 self.pic[i][j].setText("%d-%d" % (i, j))
-                self.pic[i][j].setMouseTracking(True)
                 self.pic_start_x[i][j] = self.pic_start_y[i][j] = 0
                 self.pic_middle_x[i][j] = self.pic_middle_y[i][j] = 0
                 self.pic_end_x[i][j] = self.pic_end_y[i][j] = 0
         # pic_cnt
         self.pic_cnt = [0] * (MAXIMUM_PAGE + 1)
         self.pic_ith = self.pic_jth = 1
+
+        # 畫圖透明canvas
+        self.transparent_pix = [ [None] * (MAXIMUM_PIC + 1) for i in range(MAXIMUM_PAGE + 1) ]
+        for i in range(1, MAXIMUM_PAGE + 1):
+            for j in range(1, (MAXIMUM_PIC + 1)):
+                self.transparent_pix[i][j] = QtGui.QPixmap(512, 512)
+                self.transparent_pix[i][j].fill(Qt.transparent)
+
         # 暫時試試放照片
         self.showPic(1, 1, "01372635","5F327951")
         self.showPic(1, 2, "01372635","5F327951")
